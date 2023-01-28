1936f2aebed883e5e3ee6748ba1da6391c289dca
Jason Pavlopoulos <Jasonpavlop1@gmail.com>
2023-01-28 16:32:50 +0200

    Update scheduler.c

--- scheduler/scheduler.c
+++ scheduler/scheduler.c
@@ -228,14 +228,15 @@ void round_robin(queue *q, int quantum) {
 				kill(current_proc->pid, SIGSTOP);
 				printf("1\n");
 				if(wait(&status) > 0){
+					printf("%s", current_proc->state);
 					if(WIFEXITED(status)){
+						printf("%s test", current_proc->state);
 						strcpy(current_proc->state, "EXITED");
 					}
 				}else{
 					strcpy(current_proc->state, "STOPPED");
 					enqueue(q, current_proc);
 				}
-				printf("%s", current_proc->state);
 			}
 		}
 		print(q);
