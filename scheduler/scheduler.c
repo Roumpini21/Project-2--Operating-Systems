47a4b57247ffb3ab55ecc367c31964f87b548659
Jason Pavlopoulos <Jasonpavlop1@gmail.com>
2023-01-30 23:04:56 +0200

    Update scheduler.c

--- scheduler/scheduler.c
+++ scheduler/scheduler.c
@@ -221,10 +221,10 @@ void round_robin(queue *q, int quantum){
 			if(!strcmp(current_proc->state, "RUNNING")){
 				printf("Hi from process %d\n", current_proc->pid);
 				enqueue(q, current_proc);
-				print(q);
+				print("%d\n", current_proc->pid);
 				strcpy(current_proc->state, "STOPPED");
 				nanosleep(&tim, &tim2);
-				printf("Time elapsed.");
+				printf("Time elapsed.\n");
 				/*
 				kill(current_proc->pid, SIGCONT);
 				
